98d128540e7f6f7ebc97e579171cfdacfdb742dc
Joseph J.C. Tang <jinntrance@gmail.com>
2017-10-15 03:23:10 +0800

    update url

--- Casks/font-hanazono-mincho.rb
+++ Casks/font-hanazono-mincho.rb
@@ -2,11 +2,10 @@ cask 'font-hanazono-mincho' do
   version  '20170904'
   sha256 :no_check
 
-  url "https://osdn.net/projects/hanazono-font/downloads/68253/hanazono-#{version}.zip"
+  url "http://gigenet.dl.osdn.jp/hanazono-font/68253/hanazono-#{version}.zip"
   name 'Hanazono Mincho'
   homepage 'http://fonts.jp/hanazono/'
 
   font "hanazono-#{version}/HanaMinA.ttf"
   font "hanazono-#{version}/HanaMinB.ttf"
 end
-
