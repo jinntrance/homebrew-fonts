9c0a16bd6283a3a6991d0b989483652f95feacde
Joseph J.C. Tang <jinntrance@gmail.com>
2017-10-15 03:53:49 +0800

    remove useless line

--- Casks/font-fangzheng-shusong-gbk.rb
+++ Casks/font-fangzheng-shusong-gbk.rb
@@ -2,7 +2,6 @@ cask 'font-fangzheng-shusong-gbk' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
   url 'http://cdn1.foundertype.com/Public/Uploads/ttf/FZSSK.TTF'
   name 'Fangzheng Shusong GBK'
   homepage 'http://www.foundertype.com/index.php/FontInfo/index.html?id=151'
--- Casks/font-fangzheng-shusong-sc.rb
+++ Casks/font-fangzheng-shusong-sc.rb
@@ -2,7 +2,6 @@ cask 'font-fangzheng-shusong-sc' do
   version :latest
   sha256 :no_check
 
-  # noto-website.storage.googleapis.com was verified as official when first introduced to the cask
   url 'http://cdn1.foundertype.com/Public/Uploads/ttf/FZSSJW.TTF'
   name 'Fangzheng Shusong SC'
   homepage 'http://www.foundertype.com/index.php/FontInfo/index.html?id=151'
