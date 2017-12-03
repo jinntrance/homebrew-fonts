e006eda7706e17bc65d00ea562df9ee3819bf716
Joseph J.C. Tang <jinntrance@gmail.com>
2017-12-03 23:38:11 +0800

    更新字体信息

--- Casks/font-microsoft-jhenghei.rb
+++ Casks/font-microsoft-jhenghei.rb
@@ -2,10 +2,11 @@ cask 'font-microsoft-jhzenghei' do
   version :latest
   sha256 :no_check
 
+  # mediafire.com was verified as official when first introduced to the cask
   url 'http://download1513.mediafire.com/p70ngh8w9wug/1gcie4s59z805m3/msjh_6.10_msjhbd_6.10.rar'
   name '微軟正黑體'
   name 'Microsoft JhengHei'
-  homepage 'https://haoran.wang/mac-os-x-install-microsoft-yahei.html'
+  homepage 'https://www.microsoft.com/typography/fonts/family.aspx?FID=368'
 
   font 'msjh.ttc'
   font 'msjhbd.ttc'
--- Casks/font-microsoft-yahei.rb
+++ Casks/font-microsoft-yahei.rb
@@ -2,7 +2,7 @@ cask 'font-microsoft-yahei' do
   version :latest
   sha256 :no_check
 
-  url 'https://haoran.wang/wp-content/uploads/2016/10/微软雅黑字体包.zip'
+  url 'https://haoran.wang/wp-content/uploads/2016/10/\u{5fae}\u{8f6f}\u{96c5}\u{9ed1}\u{5b57}\u{4f53}\u{5305}.zip'
   name '微软雅黑'
   name 'Microsoft YaHei'
   homepage 'https://haoran.wang/mac-os-x-install-microsoft-yahei.html'
