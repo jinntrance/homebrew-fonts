debd3b4036b7110a21530cbf13b59a82fe47a76a
Joseph J.C. Tang <jinntrance@gmail.com>
2017-12-03 23:40:31 +0800

    use URI instead

--- Casks/font-microsoft-yahei.rb
+++ Casks/font-microsoft-yahei.rb
@@ -2,7 +2,7 @@ cask 'font-microsoft-yahei' do
   version :latest
   sha256 :no_check
 
-  url 'https://haoran.wang/wp-content/uploads/2016/10/\u{5fae}\u{8f6f}\u{96c5}\u{9ed1}\u{5b57}\u{4f53}\u{5305}.zip'
+  url 'https://haoran.wang/wp-content/uploads/2016/10/%E5%BE%AE%E8%BD%AF%E9%9B%85%E9%BB%91%E5%AD%97%E4%BD%93%E5%8C%85.zip'
   name '微软雅黑'
   name 'Microsoft YaHei'
   homepage 'https://haoran.wang/mac-os-x-install-microsoft-yahei.html'
